d2f531fff7930d5312fb867a337d41da75c2ce85
guimendel <guimendeln@gmail.com>
2021-03-01 14:40:43 +0100

    fix/ valores negativos nao invertem as operacoes
    agora os valores passam por um abs() antes de saques e depositos

--- lib/rocketpay/accounts/operation.ex
+++ lib/rocketpay/accounts/operation.ex
@@ -4,6 +4,7 @@ defmodule Rocketpay.Accounts.Operation do
 
   def call(%{"id" => id, "value" => value}, operation) do
     operation_name = account_operation_name(operation)
+
     Multi.new()
     # verifica se a conta existe
     |> Multi.run(operation_name, fn repo, _changes -> get_account(repo, id) end)
@@ -15,6 +16,7 @@ defmodule Rocketpay.Accounts.Operation do
 
   defp get_account(repo, id) do
     IO.puts(repo)
+
     case repo.get(Account, id) do
       nil -> {:error, "Account not found"}
       account -> {:ok, account}
@@ -35,8 +37,8 @@ defmodule Rocketpay.Accounts.Operation do
     |> handle_cast(balance, operation)
   end
 
-  defp handle_cast({:ok, value}, balance, :deposit), do: Decimal.add(balance, value)
-  defp handle_cast({:ok, value}, balance, :withdraw), do: Decimal.sub(balance, value)
+  defp handle_cast({:ok, value}, balance, :deposit), do: Decimal.add(balance, Decimal.abs(value))
+  defp handle_cast({:ok, value}, balance, :withdraw), do: Decimal.sub(balance, Decimal.abs(value))
   defp handle_cast(:error, _balance, _operation), do: {:error, "Invalid deposit value"}
 
   defp update_account({:error, _reason} = error, _repo, _account), do: error
